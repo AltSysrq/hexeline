071bcf233b6c89093ee4ca6f8b0f59ec90e1dd93
Jason Lingle <jason@lin.gl>
2017-05-24 20:03:23 -0600

    Clean obsolete sources.
    - Not going to be using libpraefectus.
    
    - Old physics system does not conform to new design, even libpraefectus aside.

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,12 @@
 // OF  CONTRACT, NEGLIGENCE  OR OTHER  TORTIOUS ACTION,  ARISING OUT  OF OR  IN
 // CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-#![feature(btree_range,collections_bound)]
-
 extern crate libc;
 extern crate sdl2;
 extern crate gl;
 extern crate cgmath as cg;
 extern crate smallvec;
 
-mod praef;
-mod physics;
 mod graphic;
 
 use std::io;
